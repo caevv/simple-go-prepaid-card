148de43ebad708e11563e8d7ba2e3e394dc82fa2
Carlos Vieira <caevieira@gmail.com>
2018-08-31 11:46:56 +0100

    safe panic

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-		"github.com/caevv/simple-go-prepaid-card/service"
+	"github.com/caevv/simple-go-prepaid-card/service"
+	"runtime/debug"
+	"log"
 )
 
 func main() {
-	// defer func() {
-	// 	if r := recover(); r != nil {
-	// 		log.Fatal(r)
-	// 	}
-	// }()
-
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("%s: %s", r, string(debug.Stack()))
+		}
+	}()
 	service.Start()
 }
